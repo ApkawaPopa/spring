56a99cdcd857fced1871954755d471a8f544e58a
Arkadii Minchakov <ark1357949@gmail.com>
2024-09-10 13:59:43 +0300

    Add lesson16

--- src/main/java/minchakov/arkadii/spring/controller/FirstController.java
+++ src/main/java/minchakov/arkadii/spring/controller/FirstController.java
@@ -1,6 +1,7 @@
 package minchakov.arkadii.spring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,9 +12,10 @@ public class FirstController {
     @GetMapping("/hello")
     private String hello(
         @RequestParam(value = "name", required = false) String name,
-        @RequestParam(value = "surname", required = false) String surname
+        @RequestParam(value = "surname", required = false) String surname,
+        Model model
     ) {
-        System.out.println("Hello, " + name + " " + surname);
+        model.addAttribute("greeting", "Hello, " + name + " " + surname);
 
         return "first/hello";
     }
@@ -21,5 +23,25 @@ public class FirstController {
     @GetMapping("/goodbye")
     private String goodbye() {
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    private String calculator(
+        @RequestParam("a") int a,
+        @RequestParam("b") int b,
+        @RequestParam("action") String action,
+        Model model
+    ) {
+        double calculatedResult = switch (action) {
+            case "add" -> a + b;
+            case "sub" -> a - b;
+            case "mul" -> a * b;
+            case "div" -> a / (double) b;
+            default -> 0;
+        };
+
+        model.addAttribute("calculatedResult", calculatedResult);
+
+        return "first/calculator";
     }
 }
