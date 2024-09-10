7ab8f4d4c54db5d6afe9a5fbebdbe7b2d7f9a282
Arkadii Minchakov <ark1357949@gmail.com>
2024-09-10 13:13:29 +0300

    Add lesson15 task with annotation and new link in template

--- src/main/java/minchakov/arkadii/spring/controller/FirstController.java
+++ src/main/java/minchakov/arkadii/spring/controller/FirstController.java
@@ -3,17 +3,16 @@ package minchakov.arkadii.spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    private String hello(HttpServletRequest request) {
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
-
+    private String hello(
+        @RequestParam(value = "name", required = false) String name,
+        @RequestParam(value = "surname", required = false) String surname
+    ) {
         System.out.println("Hello, " + name + " " + surname);
 
         return "first/hello";
