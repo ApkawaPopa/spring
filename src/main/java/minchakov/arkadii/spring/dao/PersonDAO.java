4a66f963f769ea25e67f17c46f0efd092077e03d
Arkadii Minchakov <ark1357949@gmail.com>
2024-09-21 15:20:47 +0300

    Add lesson24

--- src/main/java/minchakov/arkadii/spring/dao/PersonDAO.java
+++ src/main/java/minchakov/arkadii/spring/dao/PersonDAO.java
@@ -4,11 +4,8 @@ import minchakov.arkadii.spring.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 @Component
@@ -50,21 +47,5 @@ public class PersonDAO {
 
     public void delete(int id) {
         jdbcTemplate.update("delete from person where id=?", id);
-    }
-}
-
-// можно использовать в случае, если имена полей в таблице и в сущности отличаются с точностью до snake_case и camelCase
-class PersonRowMapper implements RowMapper<Person> {
-
-    @Override
-    public Person mapRow(ResultSet resultSet, int rowNum) throws SQLException {
-        Person person = new Person();
-
-        person.setId(resultSet.getInt("id"));
-        person.setName(resultSet.getString("name"));
-        person.setAge(resultSet.getInt("age"));
-        person.setEmail(resultSet.getString("email"));
-
-        return person;
     }
 }
