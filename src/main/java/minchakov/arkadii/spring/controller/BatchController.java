8e5ee8f365f64bc7983786df67d0d52743dab34c
Arkadii Minchakov <ark1357949@gmail.com>
2024-09-21 16:58:20 +0300

    Add lesson25

---
+++ src/main/java/minchakov/arkadii/spring/controller/BatchController.java
@@ -0,0 +1,46 @@
+package minchakov.arkadii.spring.controller;
+
+import minchakov.arkadii.spring.dao.PersonDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/batch")
+public class BatchController {
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public BatchController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping
+    public String index() {
+        return "batch/index";
+    }
+
+    @GetMapping("/with_batch")
+    public String withBatch(RedirectAttributes attributes) {
+        long begin = System.currentTimeMillis();
+        personDAO.batchInsert(personDAO.make1000People());
+        long time = System.currentTimeMillis() - begin;
+
+        attributes.addFlashAttribute("executionTime", time);
+
+        return "redirect:/people";
+    }
+
+    @GetMapping("/without_batch")
+    public String withoutBatch(RedirectAttributes attributes) {
+        long begin = System.currentTimeMillis();
+        personDAO.multipleInsert(personDAO.make1000People());
+        long time = System.currentTimeMillis() - begin;
+
+        attributes.addFlashAttribute("executionTime", time);
+
+        return "redirect:/people";
+    }
+}
--- src/main/java/minchakov/arkadii/spring/dao/PersonDAO.java
+++ src/main/java/minchakov/arkadii/spring/dao/PersonDAO.java
@@ -2,11 +2,16 @@ package minchakov.arkadii.spring.dao;
 
 import minchakov.arkadii.spring.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @Component
 public class PersonDAO {
@@ -47,5 +52,53 @@ public class PersonDAO {
 
     public void delete(int id) {
         jdbcTemplate.update("delete from person where id=?", id);
+    }
+
+    public void batchInsert(List<Person> people) {
+        jdbcTemplate.batchUpdate(
+            "insert into person values(?, ?, ?, ?)",
+            new BatchPreparedStatementSetter() {
+                @Override
+                public void setValues(PreparedStatement statement, int i) throws SQLException {
+                    Person person = people.get(i);
+                    statement.setInt(1, person.getId());
+                    statement.setString(2, person.getName());
+                    statement.setInt(3, person.getAge());
+                    statement.setString(4, person.getEmail());
+                }
+
+                @Override
+                public int getBatchSize() {
+                    return people.size();
+                }
+            }
+        );
+    }
+
+    public void multipleInsert(List<Person> people) {
+        for (var person : people) {
+            add(person);
+        }
+    }
+
+    public List<Person> make1000People() {
+        Random random = new Random();
+        int batchId = random.nextInt(0, Integer.MAX_VALUE);
+
+        List<Person> people = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            int personAge = random.nextInt(120);
+            String suffix = batchId + "_" + i;
+            people.add(
+                new Person(
+                    random.nextInt(0, Integer.MAX_VALUE),
+                    "Person " + suffix,
+                    personAge,
+                    "person" + suffix + "@internet.ru"
+                )
+            );
+        }
+
+        return people;
     }
 }
