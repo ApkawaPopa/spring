caa0a27b995c839fb69eca0808b22c5c16f97689
Arkadii Minchakov <ark1357949@gmail.com>
2024-12-08 16:10:29 +0300

    Add lesson37 Проверяем Session::get на сущности Item

--- src/main/java/minchakov/arkadii/spring/App.java
+++ src/main/java/minchakov/arkadii/spring/App.java
@@ -3,7 +3,6 @@ package minchakov.arkadii.spring;
 import minchakov.arkadii.spring.model.Item;
 import minchakov.arkadii.spring.model.Person;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.query.MutationQuery;
 
 public class App {
     public static void main(String[] args) {
@@ -14,50 +13,8 @@ public class App {
 
             session.beginTransaction();
 
-            var person = session.get(Person.class, 3);
-            System.out.println(person);
-
-            var items = person.getItems();
-
-            System.out.println("Акутальные вещи, сразу после запроса: " + items);
-
-            var item1 = items.getFirst();
-            if (item1 != null) {
-                session.remove(item1);
-            }
-
-            System.out.println("Вещи после удаления первой из списка, внутри транзакции: " + person.getItems());
-
-            var item2 = items.get(1);
-            if (item2 != null) {
-                session.remove(item2);
-            }
-
-            session.getTransaction().commit();
-
-            System.out.println("Вещи после удаления первых двух из списка, снаружи транзакции: " + person.getItems());
-
-            session = sessionFactory.getCurrentSession();
-
-            session.beginTransaction();
-
-            MutationQuery query = session.createMutationQuery("update Item set id = :oldId where id = :newId");
-
-            if (item1 != null) {
-                var oldId = item1.getId();
-                var newId = session.merge(item1).getId();
-                query.setParameter("oldId", oldId);
-                query.setParameter("newId", newId);
-                query.executeUpdate();
-            }
-
-            if (item2 != null) {
-                var oldId = item2.getId();
-                var newId = session.merge(item2).getId();
-                query.setParameter("oldId", oldId);
-                query.setParameter("newId", newId);
-                query.executeUpdate();
-            }
+            var item = session.get(Item.class, 1);
+            System.out.println(item.getPerson());
 
             session.getTransaction().commit();
         }
