51b9a1c67b438e920d1f017eeff1ad96dd34635a
Arkadii Minchakov <ark1357949@gmail.com>
2024-12-08 16:29:40 +0300

    Add lesson37 Удаление дочерних записей и актуализация поля из родительской записи

--- src/main/java/minchakov/arkadii/spring/App.java
+++ src/main/java/minchakov/arkadii/spring/App.java
@@ -4,8 +4,6 @@ import minchakov.arkadii.spring.model.Item;
 import minchakov.arkadii.spring.model.Person;
 import org.hibernate.cfg.Configuration;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class App {
     public static void main(String[] args) {
@@ -16,14 +14,17 @@ public class App {
 
             session.beginTransaction();
 
-            var person = new Person("Аркадий", 21);
+            var person = session.get(Person.class, 3);
 
-            var item = new Item("Курс по Spring", person);
+            for (var item : person.getItems()) {
+                session.remove(item);
+            }
 
-            person.setItems(new ArrayList<>(List.of(item)));
+            System.out.println("Поле с вещами человека после удаления их всех: " + person.getItems());
 
-            session.persist(person);
-            session.persist(item);
+            person.getItems().clear();
+
+            System.out.println("После ручной очистки: " + person.getItems());
 
             session.getTransaction().commit();
         }
